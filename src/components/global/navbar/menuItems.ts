637bb6886c88daaab45f703d6eff6dcf888d8de5
Kristijan Federer <94006870+federerkristijan@users.noreply.github.com>
2026-04-15 15:06:14 +0200

    update: src/components/global/navbar/menuItems.ts [1674ceca]

--- src/components/global/navbar/menuItems.ts
+++ src/components/global/navbar/menuItems.ts
@@ -1,5 +1,39 @@
-export const menuItems = [
-  { title: 'Mail', url: '/contact' },
-  { title: 'LinkedIn', url: 'https://www.linkedin.com/in/kristijan-federer/' },
-  { title: 'GitHub', url: 'https://github.com/federerkristijan' },
+'use client'
+
+import { FC } from 'react';
+import { motion } from 'framer-motion';
+import { FaHome, FaInfoCircle, FaEnvelope } from 'react-icons/fa';
+import clsx from 'clsx';
+
+interface MenuItem {
+  name: string;
+  href: string;
+  icon: JSX.Element;
+}
+
+const menuItems: MenuItem[] = [
+  { name: 'Home', href: '/', icon: <FaHome /> },
+  { name: 'About', href: '/about', icon: <FaInfoCircle /> },
+  { name: 'Contact', href: '/contact', icon: <FaEnvelope /> },
 ];
+
+const MenuItems: FC = () => {
+  return (
+    <ul className="flex space-x-4">
+      {menuItems.map((item) => (
+        <motion.li
+          key={item.name}
+          whileHover={{ scale: 1.1 }}
+          className={clsx('text-gray-800', 'hover:text-blue-500')}
+        >
+          <a href={item.href} className="flex items-center space-x-2">
+            {item.icon}
+            <span>{item.name}</span>
+          </a>
+        </motion.li>
+      ))}
+    </ul>
+  );
+};
+
+export default MenuItems;
